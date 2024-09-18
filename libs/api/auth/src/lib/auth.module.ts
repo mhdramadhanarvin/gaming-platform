8cd23d733193a83f20bb9872381608da446b745b
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2024-09-19 00:10:37 +0700

    feat: remove unused

--- libs/api/auth/src/lib/auth.module.ts
+++ libs/api/auth/src/lib/auth.module.ts
@@ -5,15 +5,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt-strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {
-  RefreshToken,
-  Users,
-} from '@gaming-platform/api/shared/database/entity';
+import { RefreshToken } from '@gaming-platform/api/shared/database/entity';
 import { UsersModule } from '@gaming-platform/api/users';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Users, RefreshToken]),
+    TypeOrmModule.forFeature([RefreshToken]),
     JwtModule.register(jwtConfig),
     UsersModule,
   ],
